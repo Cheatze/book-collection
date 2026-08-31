0c27db7983b10983da4482593abc3b3b936ed962
cheatze <frolicsomequipster@gmail.com>
2026-08-31 14:38:57 +0200

    Een soort store

--- resources/js/services/store/index.ts
+++ resources/js/services/store/index.ts
@@ -1,16 +1,21 @@
 import { ref, computed } from "vue";
+import { getRequest, postRequest } from "../http";
 
 export const storeModuleFactory = (moduleName) => {
     const state = ref({});
 
     const getters = {
         all: computed(() => state.value),
+        getById: (id) => computed(() => state.value[id]),
     };
 
     const setters = {
         setAll: (items) => {
             for (const item of items)
                 state.value[item.id] = Object.freeze(item);
+        },
+        deleteByItem: (item) => {
+            delete state.value[item.id];
         },
     };
 
@@ -19,6 +24,20 @@ export const storeModuleFactory = (moduleName) => {
             const { data } = await getRequest(moduleName);
             if (!data) return;
             setters.setAll(data);
+        },
+        create: async (item) => {
+            const { data } = await postRequest(moduleName, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+        update: async (id, item) => {
+            const { data } = await putRequest(`${moduleName}/${id}`, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+        delete: async (id) => {
+            await deleteRequest(`${moduleName}/${id}`);
+            setters.deleteByItem({ id });
         },
     };
 
